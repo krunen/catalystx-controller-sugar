cfb2728cc4a91673bf98a1ce7e4116ca9e9f2912
Jan Henning Thorsen <jhthorsen@cpan.org>
2009-08-06 16:02:21 +0200

    initial commit

---
+++ lib/CatalystX/Controller/Sugar.pm
@@ -0,0 +1,48 @@
+package CatalystX::Controller::Sugar;
+
+=head1 NAME
+
+CatalystX::Controller::Sugar - Extra sugar for Catalyst controller
+
+=cut
+
+use Moose;
+
+=head1 METHODS
+
+=head2 stash
+
+=cut
+
+sub stash {
+}
+
+=head2 session
+
+=cut
+
+sub session {
+}
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-catalystx-controller-sugar at rt.cpan.org>, or through the web
+interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CatalystX-Controller-Sugar>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Jan Henning Thorsen, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jan Henning Thorsen, C<< <jhthorsen at cpan.org> >>
+
+=cut
+
+1;
