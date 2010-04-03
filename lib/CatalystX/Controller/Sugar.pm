5f84abc6b9c2e2ab4ce471e8a173fbcd36429fc0
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-04-03 12:50:45 +0200

    0.0701   Sat Apr  3 12:50:41 CEST 2010        - fix t/99-pod*.t

--- lib/CatalystX/Controller/Sugar.pm
+++ lib/CatalystX/Controller/Sugar.pm
@@ -6,7 +6,7 @@ CatalystX::Controller::Sugar - Sugar for Catalyst controller
 
 =head1 VERSION
 
-0.07
+0.0701
 
 =head1 DESCRIPTION
 
@@ -130,7 +130,7 @@ Moose::Exporter->setup_import_methods(
     also => 'Moose',
 );
 
-our $VERSION = '0.07';
+our $VERSION = '0.0701';
 our $ROOT = 'root'; # will be deprecated
 our $DEFAULT = 'default'; # will be deprecated
 our($RES, $REQ, $SELF, $CONTEXT, %CAPTURED);
--- t/99-pod.t
+++ t/99-pod.t
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 use lib qw(lib);
 use Test::More;
-eval 'use Test::Pod or plan skip_all => 'Test::Pod required';
+eval 'use Test::Pod; 1' or plan skip_all => 'Test::Pod required';
 all_pod_files_ok();
